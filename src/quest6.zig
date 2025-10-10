13d530575536ff40e1c7c09e2f213b8840de0e6d
Dave King <tildedave@gmail.com>
2025-10-10 05:04:41 -0400

    Quest 6 done - incredibly ugly

--- src/quest6.zig
+++ src/quest6.zig
@@ -31,6 +31,40 @@ fn freeMap(map: *std.StringHashMap(std.ArrayList([]const u8)), allocator: std.me
     map.deinit();
 }
 
+const SearchNode = struct {
+    distance: u32,
+    pred: ?[]const u8,
+};
+
+fn getPath(allocator: std.mem.Allocator, node: []const u8, search_map: *std.StringHashMap(SearchNode), final_answer: bool) !struct { []const u8, u32 } {
+    var result_string = try std.ArrayList(u8).initCapacity(allocator, 0);
+    defer result_string.deinit(allocator);
+
+    const writer = result_string.writer(allocator);
+    try std.fmt.format(writer, "@", .{});
+
+    var curr: ?[]const u8 = node;
+    var len: u32 = 0;
+    while (curr != null) {
+        if (final_answer) {
+            try std.fmt.format(writer, "{c}", .{curr.?[0]});
+        } else {
+            for (0..curr.?.len) |i| {
+                try std.fmt.format(writer, "{c}", .{curr.?[curr.?.len - 1 - i]});
+            }
+        }
+        curr = search_map.get(curr.?).?.pred;
+        len += 1;
+    }
+
+    const result = try allocator.alloc(u8, result_string.items.len);
+    for (0..result.len) |i| {
+        result[result.len - i - 1] = result_string.items[i];
+    }
+
+    return struct { []const u8, u32 }{ result, len };
+}
+
 pub fn answer1(allocator: std.mem.Allocator, lines: []const u8) ![]const u8 {
     var map = try parseLines(lines, allocator);
     defer freeMap(&map, allocator);
@@ -42,55 +76,69 @@ pub fn answer1(allocator: std.mem.Allocator, lines: []const u8) ![]const u8 {
     defer stack.deinit(allocator);
     try stack.append(allocator, "RR");
 
-    var distance_map = std.StringHashMap(u32).init(allocator);
-    defer distance_map.deinit();
-    try distance_map.put("RR", 0);
+    var search_map = std.StringHashMap(SearchNode).init(allocator);
+    defer search_map.deinit();
+    try search_map.put("RR", SearchNode{ .distance = 0, .pred = null });
 
-    var pred_map = std.StringHashMap([]const u8).init(allocator);
-    defer pred_map.deinit();
-
-    var lowest_distance: u32 = std.math.maxInt(u32);
-    var lowest_one: []const u8 = "RR";
+    var paths = std.StringHashMap(struct { []const u8, u32 }).init(allocator);
 
     while (stack.items.len > 0) {
         const prev = stack.pop().?;
-        const prev_distance = distance_map.get(prev).?;
+        std.debug.print("beep {s} {any}\n", .{ prev, map.get(prev) });
+        if (map.get(prev) == null) {
+            continue;
+        }
+
+        const search_node = search_map.get(prev).?;
         const children = map.get(prev).?;
         for (children.items) |child| {
             if (std.mem.eql(u8, child, "@")) {
-                if (prev_distance < lowest_distance) {
-                    lowest_one = prev;
-                    lowest_distance = prev_distance;
-                }
-            } else {
+                // get path to root and put it in the
+                const arr = try getPath(allocator, prev, &search_map, false);
+                try paths.put(arr[0], struct { []const u8, u32 }{ prev, arr[1] });
+            } else if (std.mem.eql(u8, child, "BUG")) {} else if (std.mem.eql(u8, child, "ANT")) {} else {
                 std.debug.print("curr {s} child {s}\n", .{ prev, child });
                 try stack.append(allocator, child);
-                try distance_map.put(child, prev_distance + 1);
-                try pred_map.put(child, prev);
+                try search_map.put(child, SearchNode{ .distance = search_node.distance + 1, .pred = prev });
             }
         }
     }
 
-    std.debug.print("lowest distance is {s}\n", .{lowest_one});
+    var counts = std.AutoHashMap(u32, u32).init(allocator);
+    defer counts.deinit();
 
-    var result_string = try std.ArrayList(u8).initCapacity(allocator, 0);
-    defer result_string.deinit(allocator);
-
-    const writer = result_string.writer(allocator);
-    try std.fmt.format(writer, "@", .{});
-
-    var curr: ?[]const u8 = lowest_one;
-    while (curr != null) {
-        try std.fmt.format(writer, "{s}", .{curr.?});
-        curr = pred_map.get(curr.?);
+    var paths_it = paths.valueIterator();
+    while (paths_it.next()) |v| {
+        const path_length = v.*[1];
+        if (counts.get(path_length) == null) {
+            try counts.put(path_length, 1);
+        } else {
+            try counts.put(path_length, counts.get(path_length).? + 1);
+        }
     }
 
-    const result = try allocator.alloc(u8, result_string.items.len);
-    for (0..result.len) |i| {
-        result[result.len - i - 1] = result_string.items[i];
+    var counts_it = counts.keyIterator();
+    var unique_path_length: ?u32 = null;
+    while (counts_it.next()) |v| {
+        if (counts.get(v.*) == 1) {
+            unique_path_length = v.*;
+            break;
+        }
     }
 
-    return result;
+    std.debug.print("unique length {d}\n", .{unique_path_length.?});
+    // must clean everything
+    var key_it = paths.keyIterator();
+    while (key_it.next()) |k| {
+        const path = paths.get(k.*);
+        if (path.?[1] == unique_path_length) {
+            const p = try getPath(allocator, path.?[0], &search_map, true);
+
+            return p[0];
+        }
+    }
+
+    return "joe";
 }
 
 test "example" {
