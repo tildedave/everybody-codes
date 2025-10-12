5a0d64e04c8a8d83c536492c1b3a6b78cc810e62
Dave King <tildedave@gmail.com>
2025-10-12 12:37:44 -0400

    Quest 7 - part 2

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
-// const quest1 = @import("quest1.zig");
-// const quest2 = @import("quest2.zig");
-// const quest3 = @import("quest3.zig");
-// const quest4 = @import("quest4.zig");
-// const quest5 = @import("quest5.zig");
-// const quest6 = @import("quest6.zig");
+const quest1 = @import("quest1.zig");
+const quest2 = @import("quest2.zig");
+const quest3 = @import("quest3.zig");
+const quest4 = @import("quest4.zig");
+const quest5 = @import("quest5.zig");
+const quest6 = @import("quest6.zig");
 const quest7 = @import("quest7.zig");
 const util = @import("util.zig");
 
@@ -21,20 +21,20 @@ pub fn main() !void {
     const lines = try input_file.readToEndAlloc(allocator, 2_000_000);
     defer allocator.free(lines);
 
-    // var splits = std.mem.splitScalar(u8, lines, '\n');
+    var splits = std.mem.splitScalar(u8, lines, '\n');
 
-    // if (std.mem.eql(u8, quest, "quest1")) {
-    //     const line = splits.first();
-    //     if (std.mem.eql(u8, part, "1")) {
-    //         std.debug.print("{d}\n", .{quest1.answer1(line)});
-    //     }
-    //     if (std.mem.eql(u8, part, "2")) {
-    //         std.debug.print("{d}\n", .{quest1.answer2(line)});
-    //     }
-    //     if (std.mem.eql(u8, part, "3")) {
-    //         std.debug.print("{d}\n", .{quest1.answer3(line)});
-    //     }
-    // }
+    if (std.mem.eql(u8, quest, "quest1")) {
+        const line = splits.first();
+        if (std.mem.eql(u8, part, "1")) {
+            std.debug.print("{d}\n", .{quest1.answer1(line)});
+        }
+        if (std.mem.eql(u8, part, "2")) {
+            std.debug.print("{d}\n", .{quest1.answer2(line)});
+        }
+        if (std.mem.eql(u8, part, "3")) {
+            std.debug.print("{d}\n", .{quest1.answer3(line)});
+        }
+    }
 
     // if (std.mem.eql(u8, quest, "quest2")) {
     //     const words_line = splits.first();
@@ -55,43 +55,46 @@ pub fn main() !void {
     //     }
     // }
 
-    // if (std.mem.eql(u8, quest, "quest3")) {
-    //     if (std.mem.eql(u8, part, "1")) {
-    //         std.debug.print("{d}\n", .{try quest3.answer1(lines)});
-    //     }
-    //     if (std.mem.eql(u8, part, "3")) {
-    //         std.debug.print("{d}\n", .{try quest3.answer3(lines)});
-    //     }
-    // }
-    // if (std.mem.eql(u8, quest, "quest4")) {
-    //     if (std.mem.eql(u8, part, "1")) {
-    //         std.debug.print("{d}\n", .{quest4.answer1(lines)});
-    //     }
-    //     if (std.mem.eql(u8, part, "3")) {
-    //         std.debug.print("{d}\n", .{quest4.answer3(lines)});
-    //     }
-    // }
-    // if (std.mem.eql(u8, quest, "quest5")) {
-    //     if (std.mem.eql(u8, part, "1")) {
-    //         std.debug.print("{d}\n", .{try quest5.answer1(lines, 10, allocator)});
-    //     }
-    //     if (std.mem.eql(u8, part, "2")) {
-    //         std.debug.print("{d}\n", .{try quest5.answer2(lines, allocator)});
-    //     }
-    //     if (std.mem.eql(u8, part, "3")) {
-    //         std.debug.print("{d}\n", .{try quest5.answer3(lines, allocator)});
-    //     }
-    // }
-    // if (std.mem.eql(u8, quest, "quest6")) {
-    //     if (std.mem.eql(u8, part, "1")) {
-    //         std.debug.print("{s}\n", .{try quest6.answer(allocator, lines, false)});
-    //     } else {
-    //         std.debug.print("{s}\n", .{try quest6.answer(allocator, lines, true)});
-    //     }
-    // }
+    if (std.mem.eql(u8, quest, "quest3")) {
+        if (std.mem.eql(u8, part, "1")) {
+            std.debug.print("{d}\n", .{try quest3.answer1(lines)});
+        }
+        if (std.mem.eql(u8, part, "3")) {
+            std.debug.print("{d}\n", .{try quest3.answer3(lines)});
+        }
+    }
+    if (std.mem.eql(u8, quest, "quest4")) {
+        if (std.mem.eql(u8, part, "1")) {
+            std.debug.print("{d}\n", .{quest4.answer1(lines)});
+        }
+        if (std.mem.eql(u8, part, "3")) {
+            std.debug.print("{d}\n", .{quest4.answer3(lines)});
+        }
+    }
+    if (std.mem.eql(u8, quest, "quest5")) {
+        if (std.mem.eql(u8, part, "1")) {
+            std.debug.print("{d}\n", .{try quest5.answer1(lines, 10, allocator)});
+        }
+        if (std.mem.eql(u8, part, "2")) {
+            std.debug.print("{d}\n", .{try quest5.answer2(lines, allocator)});
+        }
+        if (std.mem.eql(u8, part, "3")) {
+            std.debug.print("{d}\n", .{try quest5.answer3(lines, allocator)});
+        }
+    }
+    if (std.mem.eql(u8, quest, "quest6")) {
+        if (std.mem.eql(u8, part, "1")) {
+            std.debug.print("{s}\n", .{try quest6.answer(allocator, lines, false)});
+        } else {
+            std.debug.print("{s}\n", .{try quest6.answer(allocator, lines, true)});
+        }
+    }
     if (std.mem.eql(u8, quest, "quest7")) {
         if (std.mem.eql(u8, part, "1")) {
             std.debug.print("{s}\n", .{try quest7.answer1(allocator, lines, 10)});
+        }
+        if (std.mem.eql(u8, part, "2")) {
+            std.debug.print("{s}\n", .{try quest7.answer2(allocator, lines, 10)});
         }
     }
 }
--- src/quest7.zig
+++ src/quest7.zig
@@ -20,7 +20,6 @@ pub fn parseTrack(allocator: std.mem.Allocator, track_lines: []const u8) ![]cons
     var y: usize = 0;
     var dx: i8 = 1;
     var dy: i8 = 0;
-    try track.append(allocator, track_lines[y * (newline_idx + 1) + x]);
 
     while (true) {
         if (x == newline_idx - 1 and dx == 1) {
@@ -47,11 +46,11 @@ pub fn parseTrack(allocator: std.mem.Allocator, track_lines: []const u8) ![]cons
             y += 1;
         }
 
+        try track.append(allocator, track_lines[y * (newline_idx + 1) + x]);
+
         if (x == 0 and y == 0 and dx == 0 and dy == -1) {
             break;
         }
-
-        try track.append(allocator, track_lines[y * (newline_idx + 1) + x]);
     }
 
     const result = try allocator.alloc(u8, track.items.len);
@@ -59,7 +58,7 @@ pub fn parseTrack(allocator: std.mem.Allocator, track_lines: []const u8) ![]cons
     return result;
 }
 
-pub fn answer1(allocator: std.mem.Allocator, lines: []const u8, duration: u32) ![]const u8 {
+pub fn answer(allocator: std.mem.Allocator, lines: []const u8, track: []const u8, numLaps: u32) ![]const u8 {
     var s = std.StringHashMap(u32).init(allocator);
     var lines_it = std.mem.splitScalar(u8, lines, '\n');
     defer s.deinit();
@@ -73,18 +72,30 @@ pub fn answer1(allocator: std.mem.Allocator, lines: []const u8, duration: u32) !
         var total_power: u32 = 0;
         var i = sep + 1;
         var round: u32 = 0;
+        var track_position: usize = 0;
 
-        while (round < duration) : (round += 1) {
-            const ch = line[i];
-            if (ch == '+') {
+        while (round < numLaps) {
+            const track_ch = track[track_position];
+            if (track_ch == '+') {
                 score += 1;
-            } else if (ch == '-') {
+            } else if (track_ch == '-') {
                 score -= 1;
-            } else if (ch == '#') {
-                score += 0;
+            } else {
+                const ch = line[i];
+                if (ch == '+') {
+                    score += 1;
+                } else if (ch == '-') {
+                    score -= 1;
+                } else if (ch == '#') {
+                    score += 0;
+                }
             }
 
             total_power += score;
+            track_position = (track_position + 1) % track.len;
+            if (track_ch == 'S') {
+                round += 1;
+            }
 
             i += 2;
             if (i > line.len) {
@@ -100,6 +111,7 @@ pub fn answer1(allocator: std.mem.Allocator, lines: []const u8, duration: u32) !
     var key_it = s.keyIterator();
     while (key_it.next()) |p| {
         try keys.append(allocator, p.*);
+        // std.debug.print("{s} - {d}\n", .{ p.*, s.get(p.*).? });
     }
 
     std.mem.sort(
@@ -118,10 +130,23 @@ pub fn answer1(allocator: std.mem.Allocator, lines: []const u8, duration: u32) !
     return result;
 }
 
+pub fn answer1(allocator: std.mem.Allocator, lines: []const u8, numLaps: u32) ![]const u8 {
+    return answer(allocator, lines, "S", numLaps);
+}
+
+pub fn answer2(allocator: std.mem.Allocator, lines: []const u8, numLaps: u32) ![]const u8 {
+    const track_lines = "S-=++=-==++=++=-=+=-=+=+=--=-=++=-==++=-+=-=+=-=+=+=++=-+==++=++=-=-=--\n-                                                                     -\n=                                                                     =\n+                                                                     +\n=                                                                     +\n+                                                                     =\n=                                                                     =\n-                                                                     -\n--==++++==+=+++-=+=-=+=-+-=+-=+-=+=-=+=--=+++=++=+++==++==--=+=++==+++-\n";
+    const track = try parseTrack(allocator, track_lines);
+    defer allocator.free(track);
+    std.debug.print("{s}\n", .{track});
+
+    return answer(allocator, lines, track, numLaps);
+}
+
 test "given example" {
     const lines = "A:+,-,=,=\nB:+,=,-,+\nC:=,-,+,+\nD:=,=,=,+\n";
     const allocator = std.testing.allocator;
-    const result = try answer1(allocator, lines, 10);
+    const result = try answer(allocator, lines, "S", 10);
     defer allocator.free(result);
     std.debug.print("{s}\n", .{result});
 
@@ -135,5 +160,19 @@ test "track parsing" {
     const result = try parseTrack(allocator, track_lines);
     defer allocator.free(result);
 
-    try expectEqualStrings("S+===++-=+=-", result);
+    try expectEqualStrings("+===++-=+=-S", result);
+}
+
+test "given example (part 2)" {
+    const lines = "A:+,-,=,=\nB:+,=,-,+\nC:=,-,+,+\nD:=,=,=,+\n";
+    const allocator = std.testing.allocator;
+
+    const track_lines = "S+===\n-   +\n=+=-+\n";
+    const track = try parseTrack(allocator, track_lines);
+    defer allocator.free(track);
+
+    const result = try answer(allocator, lines, track, 10);
+    defer allocator.free(result);
+
+    try expectEqualStrings("DCBA", result);
 }
