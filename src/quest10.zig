70eea9db739987a45d116a737578c66b367edddc
Dave King <tildedave@gmail.com>
2025-10-26 05:38:28 -0400

    Question 10 - closer; correct length + first letter finally

--- src/quest10.zig
+++ src/quest10.zig
@@ -19,49 +19,152 @@ test "base power" {
     try expectEqual(1851, wordPower("PTBVRCZHFLJWGMNS"));
 }
 
-fn solveBlock(allocator: std.mem.Allocator, grid: util.Grid, start_x: usize, start_y: usize) !void {
-    for (0..4) |dy| {
-        for (0..4) |dx| {
-            const x = start_x + dx;
-            const y = start_y + dy;
-            // OK, relevant letters are (start_x - 1, start_y + dy), (start_x - 2, start_y + dy) etc
+fn solveLetter(allocator: std.mem.Allocator, grid: util.Grid, start_x: usize, start_y: usize, dx: usize, dy: usize) !bool {
+    const x = start_x + dx;
+    const y = start_y + dy;
+    const current_idx = util.index(grid, x, y);
+    // OK, relevant letters are (start_x - 1, start_y + dy), (start_x - 2, start_y + dy) etc
 
-            const pairs = [_]usize{
-                util.index(grid, start_x - 1, y),
-                util.index(grid, start_x - 2, y),
-                util.index(grid, start_x + 4, y),
-                util.index(grid, start_x + 5, y),
-                util.index(grid, x, start_y - 1),
-                util.index(grid, x, start_y - 2),
-                util.index(grid, x, start_y + 4),
-                util.index(grid, x, start_y + 5),
-            };
+    const pairs = [_]usize{
+        util.index(grid, start_x - 1, y),
+        util.index(grid, start_x - 2, y),
+        util.index(grid, start_x + 4, y),
+        util.index(grid, start_x + 5, y),
+        util.index(grid, x, start_y - 1),
+        util.index(grid, x, start_y - 2),
+        util.index(grid, x, start_y + 4),
+        util.index(grid, x, start_y + 5),
+    };
 
-            var seen = std.AutoHashMap(u8, u32).init(allocator);
-            defer seen.deinit();
+    var seen = std.AutoHashMap(u8, u32).init(allocator);
+    defer seen.deinit();
 
-            for (pairs) |idx| {
-                const ch = grid.lines[idx];
-                if (seen.get(ch)) |v| {
-                    try seen.put(ch, v + 1);
+    var qstmark_idx: ?usize = null;
+
+    for (pairs) |idx| {
+        const ch = grid.lines[idx];
+        if (ch == '?') {
+            qstmark_idx = idx;
+        }
+        if (seen.get(ch)) |v| {
+            try seen.put(ch, v + 1);
+        } else {
+            try seen.put(ch, 1);
+        }
+    }
+
+    var letter: ?u8 = null;
+    var it = seen.iterator();
+    while (it.next()) |e| {
+        if (e.key_ptr.* == '?') {
+            continue;
+        }
+
+        if (e.value_ptr.* == 2) {
+            letter = e.key_ptr.*;
+        }
+    }
+
+    if (letter == null and seen.get('?') == 1) {
+        // need to look at everything in the row and column for the given x/y
+        var visible = std.AutoHashMap(u8, u32).init(allocator);
+        defer visible.deinit();
+
+        var unsolvable = false;
+
+        // std.debug.print("solving question mark ({d}, {d}) --> ", .{ x, y });
+
+        for (0..4) |ddx| {
+            const idx = util.index(grid, start_x + ddx, y);
+            if (idx == current_idx) {
+                continue;
+            }
+            const ch = grid.lines[idx];
+            if (ch == '.') {
+                unsolvable = true;
+                break;
+            }
+            if (visible.get(ch)) |v| {
+                try visible.put(ch, v + 1);
+                // std.debug.print("{c}", .{ch});
+            } else {
+                try visible.put(ch, 1);
+                // std.debug.print("{c}", .{ch});
+            }
+        }
+
+        for (0..4) |ddy| {
+            const idx = util.index(grid, x, start_y + ddy);
+            if (idx == current_idx) {
+                continue;
+            }
+            const ch = grid.lines[idx];
+            if (ch == '.') {
+                unsolvable = true;
+                break;
+            }
+            if (visible.get(ch)) |v| {
+                try visible.put(ch, v + 1);
+                // std.debug.print("{c}", .{ch});
+            } else {
+                try visible.put(ch, 1);
+                // std.debug.print("{c}", .{ch});
+            }
+        }
+
+        // std.debug.print("\n", .{});
+
+        if (!unsolvable) {
+            // std.debug.print("not unsolvable!\n", .{});
+
+            var seen_it = seen.iterator();
+            while (seen_it.next()) |e| {
+                const ch = e.key_ptr.*;
+                const cnt = e.value_ptr.*;
+
+                if (visible.get(ch)) |v| {
+                    try visible.put(ch, v + cnt);
+                    // std.debug.print("{c}", .{ch});
                 } else {
-                    try seen.put(ch, 1);
+                    try visible.put(ch, cnt);
+                    // std.debug.print("{c}", .{ch});
                 }
             }
 
-            var letter: ?u8 = null;
-            var key_it = seen.keyIterator();
-            while (key_it.next()) |ch| {
-                if (seen.get(ch.*) == 2) {
-                    letter = ch.*;
+            var visible_it = visible.iterator();
+            while (visible_it.next()) |e| {
+                const ch = e.key_ptr.*;
+                if (ch != '?' and e.value_ptr.* == 1) {
+                    letter = ch;
                 }
             }
 
             if (letter) |l| {
-                grid.lines[util.index(grid, x, y)] = l;
+                // std.debug.print("solved as {c}!\n", .{l});
+                grid.lines[qstmark_idx.?] = l;
             }
         }
     }
+
+    if (letter) |l| {
+        grid.lines[current_idx] = l;
+        return true;
+    }
+
+    return false;
+}
+
+fn solveBlock(allocator: std.mem.Allocator, grid: util.Grid, start_x: usize, start_y: usize) !bool {
+    var any_solved = false;
+    for (0..4) |dy| {
+        for (0..4) |dx| {
+            if (grid.lines[util.index(grid, start_x + dx, start_y + dy)] == '.') {
+                any_solved = try solveLetter(allocator, grid, start_x, start_y, dx, dy) or any_solved;
+            }
+        }
+    }
+
+    return any_solved;
 }
 
 fn readBlock(allocator: std.mem.Allocator, grid: util.Grid, start_x: usize, start_y: usize) ![]const u8 {
@@ -87,7 +190,7 @@ pub fn answer1(allocator: std.mem.Allocator, lines: []const u8) ![]const u8 {
     const grid = try util.createGrid(allocator, lines);
     defer allocator.free(grid.lines);
 
-    try solveBlock(allocator, grid, 2, 2);
+    _ = try solveBlock(allocator, grid, 2, 2);
     return readBlock(allocator, grid, 2, 2);
 }
 
@@ -100,7 +203,7 @@ pub fn answer2(allocator: std.mem.Allocator, lines: []const u8) !u64 {
     while (y <= grid.height - 6) : (y += 9) {
         var x: usize = 2;
         while (x <= grid.width - 6) : (x += 9) {
-            try solveBlock(allocator, grid, x, y);
+            _ = try solveBlock(allocator, grid, x, y);
             const word = try readBlock(allocator, grid, x, y);
             total += wordPower(word);
             allocator.free(word);
@@ -114,7 +217,42 @@ pub fn answer3(allocator: std.mem.Allocator, lines: []const u8) !u64 {
     const grid = try util.createGrid(allocator, lines);
     defer allocator.free(grid.lines);
 
-    return 0;
+    var solved_any = true;
+    while (solved_any) {
+        solved_any = false;
+        var y: usize = 2;
+        while (y <= grid.height - 6) : (y += 6) {
+            var x: usize = 2;
+            while (x <= grid.width - 6) : (x += 6) {
+                solved_any = try solveBlock(allocator, grid, x, y) or solved_any;
+            }
+        }
+    }
+
+    std.debug.print("--------\n", .{});
+    var it = std.mem.splitScalar(u8, grid.lines, '\n');
+    while (it.next()) |line| {
+        std.debug.print("{s}\n", .{line});
+    }
+
+    var total: u64 = 0;
+    var y: usize = 2;
+    while (y <= grid.height - 6) : (y += 6) {
+        var x: usize = 2;
+        while (x <= grid.width - 6) : (x += 6) {
+            const w = try readBlock(
+                allocator,
+                grid,
+                x,
+                y,
+            );
+            std.debug.print("{s}\n", .{w});
+            total += wordPower(w);
+            defer allocator.free(w);
+        }
+    }
+
+    return total;
 }
 
 test "given example (part 1)" {
@@ -136,10 +274,18 @@ test "example (part 3)" {
     const grid = try util.createGrid(allocator, lines);
     defer allocator.free(grid.lines);
 
+    _ = try solveBlock(allocator, grid, 2, 2);
+    const w = try readBlock(allocator, grid, 2, 2);
+    std.debug.print("{s}\n", .{w});
+    defer allocator.free(w);
+
+    _ = try solveBlock(allocator, grid, 2, 2);
+    const word = try readBlock(allocator, grid, 2, 2);
+    defer allocator.free(word);
+    try expectEqualStrings("LWGVXSHBPJQKNFZM", word);
+
     // const word: []u8 = try runicWord(allocator, grid, 2, 2);
     // defer std.testing.allocator.free(word);
-    // try solveWord(allocator, word, grid, 2, 2);
-    // try expectEqualStrings("LWGVXSHBPJQKNFZM", word);
 }
 
 test "full part 3" {
