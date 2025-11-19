01ad9c41fb5dff315e317e3f1a9dd3fcce9c4c8a
Dave King <tildedave@gmail.com>
2025-11-19 05:10:43 -0500

    [Story 2] Trying to max ILP work

--- 2025/everybodycodes/lib/story2.ml
+++ 2025/everybodycodes/lib/story2.ml
@@ -1,3 +1,5 @@
+[@@@ocaml.warning "-31-32"]
+
 open Base
 open Core
 open Util
@@ -140,3 +142,125 @@ let%test_unit "quest1 quest1part2 (coins won)" =
        ])
 
 (* part 3 is brute forceable *)
+
+let num_slots (Board ((xmax, _), _)) = (xmax + 1) / 2
+
+let build num_slots token_to_slot_mapping =
+  let num_tokens = Hashtbl.length token_to_slot_mapping in
+  let tokens = Array.of_list @@ Hashtbl.keys token_to_slot_mapping in
+  let open Lp in
+  let x =
+    Array.init (Array.length tokens) ~f:(fun n ->
+        Array.init num_slots ~f:(fun k ->
+            Lp.Poly.var ~integer:true ~lb:0. ~ub:1.
+              (Printf.sprintf "x_{%d%d}" n k)))
+  in
+  let obj =
+    List.fold ~init:(c 0.0) ~f:( ++ )
+      (List.map
+         ~f:(fun (n, k) ->
+           x.(n).(k)
+           *~ c
+                (* slots are 1-indexed *)
+                (float_of_int
+                   (Array.get
+                      (Hashtbl.find_exn token_to_slot_mapping tokens.(n))
+                      k)))
+         (List.cartesian_product
+            (Util.( -- ) 0 (num_tokens - 1))
+            (Util.( -- ) 0 (num_slots - 1))))
+  in
+  let token_matching =
+    List.init (Array.length tokens) ~f:(fun n ->
+        eq (c 1.0)
+          (List.fold ~f:( ++ ) ~init:(c 0.0)
+             (List.init num_slots ~f:(fun k -> x.(n).(k)))))
+  in
+  let slot_matching =
+    List.init num_slots ~f:(fun k ->
+        eq (c 1.0)
+          (List.fold ~f:( ++ ) ~init:(c 0.0)
+             (List.init num_tokens ~f:(fun n -> x.(n).(k)))))
+  in
+  (make (maximize obj) (token_matching @ slot_matching), x)
+
+(* copied from ocaml-lp, available in 0.5.0 *)
+
+let compute_term pmap t =
+  let open Lp in
+  match t with
+  | Term.Const f -> f
+  | Term.Linear (f, x) -> f *. PMap.find (Poly.of_var x) pmap
+  | Term.Quad (f, x, y) ->
+      f *. PMap.find (Poly.of_var x) pmap *. PMap.find (Poly.of_var y) pmap
+
+let compute_poly pmap p =
+  Lp.Poly.map (compute_term pmap) p |> List.fold ~f:( +. ) ~init:0.
+
+let solve num_slots token_to_slot_mapping =
+  let problem, _ = build num_slots token_to_slot_mapping in
+  Stdio.printf "%s\n" (Lp.Problem.to_string problem);
+  match Lp_glpk.Milp.solve problem with
+  | Ok (_, _) -> Stdio.printf "solved!\n"
+  | Error msg -> Stdio.printf "Failed to solve: %s\n" msg
+
+let quest1part3 l =
+  let board, tokens = parse_lines l in
+  let token_to_slot_mapping = Hashtbl.create (module String) in
+  tokens
+  |> List.iter ~f:(fun token ->
+         Hashtbl.set token_to_slot_mapping ~key:token
+           ~data:
+             (Array.init (num_slots board) ~f:(fun s ->
+                  tokens_won board token (s + 1))));
+  solve (num_slots board) token_to_slot_mapping
+
+let _ =
+  quest1part3
+    [
+      "*.*.*.*.*.*.*.*.*";
+      ".*.*.*.*.*.*.*.*.";
+      "*.*.*...*.*...*..";
+      ".*.*.*.*.*...*.*.";
+      "*.*.....*...*.*.*";
+      ".*.*.*.*.*.*.*.*.";
+      "*...*...*.*.*.*.*";
+      ".*.*.*.*.*.*.*.*.";
+      "*.*.*...*.*.*.*.*";
+      ".*...*...*.*.*.*.";
+      "*.*.*.*.*.*.*.*.*";
+      ".*.*.*.*.*.*.*.*.";
+      "";
+      "RRRLRLRRRRRL";
+      "LLLLRLRRRRRR";
+      "RLLLLLRLRLRL";
+      "LRLLLRRRLRLR";
+      "LLRLLRLLLRRL";
+      "LRLRLLLRRRRL";
+    ]
+(*
+let x = Lp.var "x"
+let y = Lp.var "y"
+
+let problem =
+  let open Lp in
+  let obj = maximize (x ++ y) in
+  let c0 = x ++ (c 1.2 *~ y) <~ c 5.0 in
+  let c1 = (c 2.0 *~ x) ++ y <~ c 1.2 in
+  make obj [ c0; c1 ]
+
+let write () = Lp.write "my_problem.lp" problem
+
+let solve () =
+  (* For other interfaces, use Lp_glpk_js or Lp_gurobi instead *)
+  match Lp_glpk.solve problem with
+  | Ok (obj, xs) ->
+      Printf.printf "Objective: %.2f\n" obj;
+      Printf.printf "x: %.2f y: %.2f\n" (Lp.PMap.find x xs) (Lp.PMap.find y xs)
+  | Error msg -> print_endline msg
+
+let () =
+  if Lp.validate problem then (
+    write ();
+    solve ())
+  else print_endline "Oops, my problem is broken." *)
