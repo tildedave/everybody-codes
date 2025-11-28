333d89cd9e5ed81f871990f28a3544afe093be2f
Dave King <tildedave@gmail.com>
2025-11-27 20:37:48 -0500

    [2025] Quest 18 - MILP implemented, there is a bug somewhere

--- 2025/everybodycodes/lib/quest18.ml
+++ 2025/everybodycodes/lib/quest18.ml
@@ -141,4 +141,120 @@ let part2 l =
               final_pid)
         test_cases
 
-let part3 _ = 1
+(* time to bust out the linear optimizer for part 3 :-) *)
+(* OK so issue with linear programming is that if the incoming energy < thickness -> plant energy is 0 *)
+(* can handle this with variables representing the plant being "on" *)
+
+(* only "gotcha" is the thickness *)
+(* p_outgoing = if sum p_incoming > thickness then sum p_incoming else 0 *)
+(* constraint outgoing energy == 0 OR outgoing_energy = inc*)
+(* plant_on * incoming energy >= thickness *)
+
+(* OK, something like this will work *)
+
+let large_c = Lp.c 1000000.0
+
+let build plants_by_id last_pid =
+  let open Lp in
+  let p_inc = Hashtbl.create (module Int) in
+  let p_out = Hashtbl.create (module Int) in
+  let y_pon = Hashtbl.create (module Int) in
+  let y_freebranches = Hashtbl.create (module Int) in
+  Map.iter_keys plants_by_id ~f:(fun pid ->
+      Hashtbl.add_exn p_inc ~key:pid ~data:(var (Printf.sprintf "p%d_inc" pid));
+      Hashtbl.add_exn p_out ~key:pid ~data:(var (Printf.sprintf "p%d_out" pid));
+      Hashtbl.add_exn y_pon ~key:pid
+        ~data:(var (Printf.sprintf "yp%d_on" pid) ~integer:true ~lb:0.0 ~ub:1.0);
+      match Map.find_exn plants_by_id pid with
+      | Plant (_, _, branches) ->
+          List.iter branches ~f:(fun branch ->
+              match branch with
+              | FreeBranch _ ->
+                  Hashtbl.add_exn y_freebranches ~key:pid
+                    ~data:
+                      (var
+                         (Printf.sprintf "free%d" pid)
+                         ~integer:true ~lb:0. ~ub:1.)
+              | _ -> ()));
+  let z = var "z" in
+  let constraints =
+    Map.fold plants_by_id
+      ~init:[ eq z (Hashtbl.find_exn p_out last_pid) ]
+      ~f:(fun ~key ~data acc ->
+        match data with
+        | Plant (_, plant_thickness, branches) ->
+            let incoming_energy =
+              List.fold ~f:( ++ ) ~init:(c 0.0)
+                (List.map
+                   ~f:(fun branch ->
+                     match branch with
+                     | FreeBranch _ -> Hashtbl.find_exn y_freebranches key
+                     | PlantBranch (branch_id, branch_thickness) ->
+                         Hashtbl.find_exn p_out branch_id
+                         *~ c (Float.of_int branch_thickness))
+                   branches)
+            in
+            let b, y_in, y_out =
+              ( Hashtbl.find_exn y_pon key,
+                Hashtbl.find_exn p_inc key,
+                Hashtbl.find_exn p_out key )
+            in
+            eq incoming_energy y_in
+            :: (y_in <~ c (Float.of_int plant_thickness) ++ (large_c *~ b))
+            :: (y_in
+               >~ c (Float.of_int plant_thickness) -- (large_c *~ (c 1. -- b)))
+            :: (y_out <~ y_in ++ (large_c *~ (c 1. -- b)))
+            :: (y_out >~ y_in -- (large_c *~ (c 1. -- b)))
+            :: (y_out <~ large_c *~ b)
+            :: (y_out >~ c 0.0)
+            :: acc)
+  in
+  Stdio.printf "%s\n"
+    (String.concat ~sep:"\n" (List.map ~f:Lp.Cnstr.to_string constraints));
+  (make (maximize z) constraints, z)
+
+(* copied from ocaml-lp, available in 0.5.0 *)
+
+let compute_term pmap t =
+  let open Lp in
+  match t with
+  | Term.Const f -> f
+  | Term.Linear (f, x) -> f *. PMap.find (Poly.of_var x) pmap
+  | Term.Quad (f, x, y) ->
+      f *. PMap.find (Poly.of_var x) pmap *. PMap.find (Poly.of_var y) pmap
+
+let compute_poly pmap p =
+  Lp.Poly.map (compute_term pmap) p |> List.fold ~f:( +. ) ~init:0.
+
+let part3 l =
+  let parsed_list = l |> split_list_on ~splitter:(equal_string "") in
+  let plants_list =
+    parsed_list
+    |> List.sub ~pos:0 ~len:(List.length parsed_list - 1)
+    |> List.map ~f:parse_plant
+  in
+  let plants_by_id =
+    List.fold_right
+      ~f:(fun plant map ->
+        match plant with
+        | Plant (pid, _, _) -> Map.add_exn map ~key:pid ~data:plant)
+      ~init:(Map.empty (module Int))
+      plants_list
+  in
+  let test_cases = List.last_exn parsed_list in
+  match List.last_exn plants_list with
+  | Plant (final_pid, _, _) -> (
+      let problem, z = build plants_by_id final_pid in
+      Stdio.printf "problem: %s\n" (Lp.Problem.to_string problem);
+      match Lp_glpk.Milp.solve problem with
+      | Ok (_, pmap) ->
+          Stdio.printf "%s\n"
+            (String.concat ~sep:"\n"
+               (List.map
+                  ~f:(fun (p, f) ->
+                    Printf.sprintf "%s=%.2f" (Lp.Poly.to_string p) f)
+                  (Lp.PMap.to_list pmap)));
+          Int.of_float (compute_poly pmap z)
+      | Error msg ->
+          Stdio.printf "Failed to solve: %s\n" msg;
+          assert false)
