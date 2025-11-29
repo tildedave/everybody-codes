7c56c6ae74f4200a81c90ade148de99676f24e03
Dave King <tildedave@gmail.com>
2025-11-29 13:59:17 -0500

    [2025] Quest 19 - part 3 - had to get some hints from reddit

--- 2025/everybodycodes/lib/quest18.ml
+++ 2025/everybodycodes/lib/quest18.ml
@@ -141,89 +141,9 @@ let part2 l =
               final_pid)
         test_cases
 
-(* time to bust out the linear optimizer for part 3 :-) *)
-(* OK so issue with linear programming is that if the incoming energy < thickness -> plant energy is 0 *)
-(* can handle this with variables representing the plant being "on" *)
-
-(* only "gotcha" is the thickness *)
-(* p_outgoing = if sum p_incoming > thickness then sum p_incoming else 0 *)
-(* constraint outgoing energy == 0 OR outgoing_energy = inc*)
-(* plant_on * incoming energy >= thickness *)
-
-(* OK, something like this will work *)
-
-let big_c = 10000000.
-let minus_c = -.big_c
-let large_c = Lp.c big_c
-let epsilon = Lp.c 0.0001
-
-let build plants_by_id last_pid =
-  let open Lp in
-  let p_inc = Hashtbl.create (module Int) in
-  let p_out = Hashtbl.create (module Int) in
-  let y_pon = Hashtbl.create (module Int) in
-  let y_freebranches = Hashtbl.create (module Int) in
-  Map.iter_keys plants_by_id ~f:(fun pid ->
-      Hashtbl.add_exn p_inc ~key:pid
-        ~data:(var (Printf.sprintf "p%d_inc" pid) ~lb:minus_c ~ub:big_c);
-      Hashtbl.add_exn p_out ~key:pid ~data:(var (Printf.sprintf "p%d_out" pid));
-      Hashtbl.add_exn y_pon ~key:pid
-        ~data:(var (Printf.sprintf "yp%d_on" pid) ~integer:true ~lb:0. ~ub:1.));
-  let z = var "z" in
-  let constraints =
-    Map.fold plants_by_id
-      ~init:[ eq z (Hashtbl.find_exn p_inc last_pid) ]
-      ~f:(fun ~key ~data acc ->
-        match data with
-        | Plant (_, plant_thickness, branches) ->
-            let incoming_energy =
-              List.fold ~f:( ++ ) ~init:(c 0.0)
-                (List.map
-                   ~f:(fun branch ->
-                     match branch with
-                     | FreeBranch _ -> Hashtbl.find_exn y_pon key
-                     | PlantBranch (branch_id, branch_thickness) ->
-                         Hashtbl.find_exn p_out branch_id
-                         *~ c (Float.of_int branch_thickness))
-                   branches)
-            in
-            let b, y_in, y_out =
-              ( Hashtbl.find_exn y_pon key,
-                Hashtbl.find_exn p_inc key,
-                Hashtbl.find_exn p_out key )
-            in
-            eq incoming_energy y_in
-            :: (y_in <~ c (Float.of_int plant_thickness) ++ (large_c *~ b))
-            :: (y_in
-               >~ c (Float.of_int plant_thickness) -- (large_c *~ (c 1. -- b)))
-            :: (y_out <~ y_in ++ (large_c *~ (c 1. -- b)))
-            :: (y_out >~ y_in -- (large_c *~ (c 1. -- b)))
-            :: (y_out <~ large_c *~ b)
-            :: (y_out >~ c 0.0)
-            :: acc)
-  in
-  Stdio.printf "%s\n"
-    (String.concat ~sep:"\n" (List.map ~f:Lp.Cnstr.to_string constraints));
-  ( make (maximize z)
-      (* ((Hashtbl.find_exn y_pon 1 =~ c 1.0)
-         :: (Hashtbl.find_exn y_pon 2 =~ c 0.0)
-         :: (Hashtbl.find_exn y_pon 3 =~ c 1.0)
-         :: constraints), *)
-      constraints,
-    z )
-
-(* copied from ocaml-lp, available in 0.5.0 *)
-
-let compute_term pmap t =
-  let open Lp in
-  match t with
-  | Term.Const f -> f
-  | Term.Linear (f, x) -> f *. PMap.find (Poly.of_var x) pmap
-  | Term.Quad (f, x, y) ->
-      f *. PMap.find (Poly.of_var x) pmap *. PMap.find (Poly.of_var y) pmap
-
-let compute_poly pmap p =
-  Lp.Poly.map (compute_term pmap) p |> List.fold ~f:( +. ) ~init:0.
+(* OK I failed to use the linear optimizer for part 3, I got pretty far *)
+(* looking at visualization on Reddit it seems like free branches are only ever
+   positive or negative *)
 
 let part3 l =
   let parsed_list = l |> split_list_on ~splitter:(equal_string "") in
@@ -233,27 +153,53 @@ let part3 l =
     |> List.map ~f:parse_plant
   in
   let plants_by_id =
-    List.fold_right
-      ~f:(fun plant map ->
+    List.fold
+      ~f:(fun map plant ->
         match plant with
         | Plant (pid, _, _) -> Map.add_exn map ~key:pid ~data:plant)
       ~init:(Map.empty (module Int))
       plants_list
   in
+  let freebranches =
+    List.fold
+      ~init:(Set.empty (module Int))
+      ~f:(fun acc (Plant (pid, _, branches)) ->
+        match branches with [ FreeBranch _ ] -> Set.add acc pid | _ -> acc)
+      plants_list
+  in
+  let positive_branches =
+    List.fold ~init:freebranches
+      ~f:(fun s plant ->
+        match plant with
+        | Plant (_, _, branches) ->
+            List.fold ~init:s
+              ~f:(fun freebranches p ->
+                match p with
+                | PlantBranch (pid, thickness) ->
+                    if thickness < 0 then Set.remove freebranches pid
+                    else freebranches
+                | _ -> freebranches)
+              branches)
+      plants_list
+  in
   let test_cases = List.last_exn parsed_list in
   match List.last_exn plants_list with
-  | Plant (final_pid, _, _) -> (
-      let problem, z = build plants_by_id final_pid in
-      Stdio.printf "problem: %s\n" (Lp.Problem.to_string problem);
-      match Lp_glpk.Milp.solve problem with
-      | Ok (_, pmap) ->
-          Stdio.printf "%s\n"
-            (String.concat ~sep:"\n"
-               (List.map
-                  ~f:(fun (p, f) ->
-                    Printf.sprintf "%s=%.2f" (Lp.Poly.to_string p) f)
-                  (Lp.PMap.to_list pmap)));
-          Int.of_float (compute_poly pmap z)
-      | Error msg ->
-          Stdio.printf "Failed to solve: %s\n" msg;
-          assert false)
+  | Plant (final_pid, _, _) ->
+      let max_power =
+        plant_power plants_by_id
+          (fun n -> Set.mem positive_branches n)
+          final_pid
+      in
+      List.fold_right ~init:0
+        ~f:(fun s acc ->
+          let branch_activations =
+            s |> String.split ~on:' ' |> List.map ~f:Int.of_string
+            |> Array.of_list
+          in
+          let power =
+            plant_power plants_by_id
+              (fun n -> branch_activations.(n - 1) = 1)
+              final_pid
+          in
+          acc + if power = 0 then 0 else max_power - power)
+        test_cases
